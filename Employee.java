802085f5276c066194f097641166675a8bb6f13d
Reema <95617133+ReemaAlonazi@users.noreply.github.com>
2022-11-21 18:29:04 +0300

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,39 @@
+public class Employee {
+    int emp_ID; //attribute
+    String name;
+    String depart;
+    double salary;
+    double bouns;
+    boolean resident;
+
+
+    public Employee(){//constructor
+
+    }
+    public Employee(int emp_ID, String name){
+
+    }
+    public Employee(int emp_ID, String name , boolean resident){
+
+    }
+    public Employee(int emp_ID, String name ,String depart,double salary ,double bouns,boolean resident){
+
+    }
+
+
+    public void setSalary(double salary){//methods
+
+    }
+    public void setSalary(double salary, double bouns){
+
+    }
+    public void setDepart(String depart){
+
+    }
+    public void setEmp_ID(int emp_ID){
+
+    }
+    public void print_Emp_Dara(){
+
+    }
+}
